70e257f5f27432f1a4e479883e776c8e001b62c6
raaakib0 <raaakib0@gmail.com>
2025-11-24 21:34:22 +0600

    brand controller added

---
+++ app/Http/Controllers/BrandController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Brand;
+use Illuminate\Http\Request;
+
+class BrandController extends Controller
+{
+    public function brands(){
+        $brands = Brand::all();
+        return view();
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -7,3 +7,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 Route::get('/generics', [GenericController::class,'index']);
+Route::get('/brand');
