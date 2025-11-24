87536ed4b18cb66038d6fd678ac30ef805da6f6d
raaakib0 <raaakib0@gmail.com>
2025-11-24 20:47:18 +0600

    generic route create

---
+++ resources/views/generics/index.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Generics List</title>
+    </head>
+    <body>
+        <h1>All Generic Data</h1>
+        <table border="1" cellpadding="5" >
+<tr>
+    <th>Id</th>
+    <th>Name</th>
+    <th>Detail</th>
+</tr>
+
+@foreach ($generics as $generic)
+    
+@endforeach
+<tr>
+    <td></td>
+    <td></td>
+    <td></td>
+</tr>
+        </table>
+    </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\GenericController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/generics', [GenericController::class,'index']);
