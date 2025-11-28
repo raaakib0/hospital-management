8b0216effb80ef3371907bd3ab39ce3ec79a178e
raaakib0 <raaakib0@gmail.com>
2025-11-28 20:24:02 +0600

    brand view table add

--- resources/views/BrandView.blade.php
+++ resources/views/BrandView.blade.php
@@ -1,3 +1,28 @@
-<div>
-    <!-- You must be the change you wish to see in the world. - Mahatma Gandhi -->
-</div>
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Brand List</title>
+</head>
+
+<body>
+    <h1>All Brand List</h1>
+    <table border="1" cellpading="5">
+        <tr>
+            <th>Id</th>
+            <th>Name</th>
+            <th>Price</th>
+        </tr>
+@foreach ($brands as $brand )
+    
+        <tr>
+            <td>{{ $brand->id }}</td>
+            <td>{{ $brand->name }}</td>
+            <td>{{ $brand->price }}</td>
+        </tr>
+@endforeach
+
+    </table>
+</body>
+
+</html>
