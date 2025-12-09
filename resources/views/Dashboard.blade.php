05665df2d1031cb323b65ef8d76209af62bd35db
raaakib0 <raaakib0@gmail.com>
2025-12-09 20:53:41 +0600

    change name welcome page to dashboard

---
+++ resources/views/Dashboard.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+@extends('layouts.app')
+@section('title', 'Welcome Page');
+
+
+<body>
+
+    @section('content')
+        <div>
+            <h1>Home Page</h1>
+        </div>
+    @endsection
+</body>
+
+</html>
