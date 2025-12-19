1e0fbedf1ec46c622ae58ad7daf685614d534244
raaakib0 <raaakib0@gmail.com>
2025-12-19 20:11:53 +0600

    write dashboard controller function

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -6,5 +6,11 @@ use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
-    //
+    public function index(){
+        $brandCount = \App\Models\Brand::count();
+        $companyCount =\App\Models\Company::count();
+        $genericCount=\App\Models\Generic::count();
+
+        return view('dashboard', compact('brandCount', 'companyCount', 'genericCount'));
+    }
 }
