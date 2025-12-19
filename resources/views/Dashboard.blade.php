44fbb99adc2174e8c7deea59a6b121e778d91a64
raaakib0 <raaakib0@gmail.com>
2025-12-19 19:51:26 +0600

    brand col add in dashboard

--- resources/views/Dashboard.blade.php
+++ resources/views/Dashboard.blade.php
@@ -9,6 +9,16 @@
     @section('content')
         <div>
             <h1>?Dashboard?</h1>
+            <hr>
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="card bg-primary text-white p-3">
+                        <h3>{{ $brandCount }}</h3>
+                        <p>Total Brands</p>
+                    </div>
+                </div>
+
+            </div>
         </div>
     @endsection
 </body>
