d0dc8ad1d62b9c416a83e1ee59670e7ece4863d6
raaakib0 <raaakib0@gmail.com>
2025-12-20 18:57:14 +0600

    make clickable brands

--- resources/views/Dashboard.blade.php
+++ resources/views/Dashboard.blade.php
@@ -4,28 +4,30 @@
 
 <body>
     @section('content')
-            <h1>?Dashboard?</h1>
-            <hr>
-            <div class="row">
-                <div class="col-md-4">
+        <h1>Dashboard</h1>
+        <hr>
+        <div class="row">
+            <div class="col-md-4">
+                <a href="/brands" class="text-decoration-none">
                     <div class="card bg-primary text-white p-3">
                         <h3>{{ $brandCount }}</h3>
                         <p>Total Brands</p>
                     </div>
-                </div>
-                <div class="col-md-4">
-                    <div class="card bg-success text-white p-3">
-                        <h3>{{ $companyCount }}</h3>
-                        <p>Total Companies</p>
-                    </div>
-                </div>
-                <div class="col-md-4">
-                    <div class="card bg-info text-white p-3">
-                        <h1>{{ $genericCount }}</h1>
-                        <p>Total Generics</p>
-                    </div>
+                </a>
+            </div>
+            <div class="col-md-4">
+                <div class="card bg-success text-white p-3">
+                    <h3>{{ $companyCount }}</h3>
+                    <p>Total Companies</p>
                 </div>
             </div>
+            <div class="col-md-4">
+                <div class="card bg-info text-white p-3">
+                    <h1>{{ $genericCount }}</h1>
+                    <p>Total Generics</p>
+                </div>
+            </div>
+        </div>
     @endsection
 </body>
 
